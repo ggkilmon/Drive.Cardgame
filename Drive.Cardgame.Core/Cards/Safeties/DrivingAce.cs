f543d8f22983f929b3cea371d8d93add0f92b01b
Graham Kilmon <graham_kilmon@hermanmiller.com>
2019-12-24 11:58:49 -0500

    add implementation classes for Safeties

---
+++ Drive.Cardgame.Core/Cards/Safeties/DrivingAce.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drive.Cardgame.Core.Cards.Safeties
+{
+    public class DrivingAce : Safety
+    {
+    }
+}
---
+++ Drive.Cardgame.Core/Cards/Safeties/PunctureProof.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drive.Cardgame.Core.Cards.Safeties
+{
+    public class PunctureProof : Safety
+    {
+    }
+}
